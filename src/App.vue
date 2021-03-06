c14e708bad54964bdc5e3dacaac6ef6ea8f2aa3b
susheng <2273244656@qq.com>
2021-03-06 10:50:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from './components/button'
+export default {
+  name: 'App',
+  components:{
+    Button
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../public/reset.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
