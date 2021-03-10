9a480606ee2d4773169bfff88e0adcc99c2c3e95
susheng <2273244656@qq.com>
2021-03-10 18:28:11 +0800

    row 和col 的雏形完成

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import  Icon from  './icon'
 import ButtonGroup from './button-group'
 import Input from "./Input";
+import Row from "./row"
+import Col from './col'
 
 Vue.config.productionTip = false
 Vue.component('g-icon',Icon)
 Vue.component('Button-group',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 new Vue({
   render: h => h(App),
 
